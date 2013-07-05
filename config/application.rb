d5f62d936b89c8e08add909c365a0e5b7f357ef9
Tom Szymanski <szymanski.tom@gmail.com>
2013-07-05 00:28:48 -0600

    updating application.rb

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,6 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
-Bundler.require(*Rails.groups(assets: %w(development test production)))
 
 module Hopesteel
   class Application < Rails::Application
