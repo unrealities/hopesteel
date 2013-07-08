3b4de7ebcc816a1c2c8d7faafc74cf218e99c92b
Tom Szymanski <szymanski.tom@gmail.com>
2013-07-08 10:05:35 -0600

    More content and ui changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,6 +21,7 @@ Hopesteel::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = true
+  config.assets.precompile += ['static_pages.css']
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
